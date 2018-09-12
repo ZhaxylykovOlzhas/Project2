1cf3d8fbb4a69cc25cb9b5fefa4a57490715fc54
zhaxylykov <zhaxylykov_ob@mail.ru>
2018-09-12 17:47:15 +0600

    first commit

---
+++ Practice2/Practice2/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //   Homework 1 и 2)
+
+            //   int numbers1;
+            //   int numbers2;
+            //   int numbers3;
+
+            //   Console.WriteLine("Введите 1- число:");
+            //   numbers1 = Convert.ToInt32(Console.ReadLine());
+
+            //   Console.WriteLine("Введите 2- число:");
+            //   numbers2 = Convert.ToInt32(Console.ReadLine());
+
+            //   Console.WriteLine("Введите 3- число:");
+            //   numbers3 = Convert.ToInt32(Console.ReadLine());
+
+
+            //   Console.WriteLine("Числы: " + numbers1 + " " + numbers2 + " " + numbers3);
+
+
+            //   Console.ReadLine();
+
+
+            //   3)
+            //  int numbers1;
+
+            //   Console.WriteLine("Введите расстояние в сантиметрах:");
+            //   numbers1 = Convert.ToInt32(Console.ReadLine());
+
+
+
+            //   Console.WriteLine("Число полных метров: " + numbers1 / 100);
+
+
+            //   Console.ReadLine();
+
+            //   4)
+
+            //   int numbers1 = 234;
+
+
+            //   Console.WriteLine("Количество полных недель : " + numbers1 / 7);
+
+
+            //   Console.ReadLine();
+
+
+
+            //   5)
+            //int numbers;
+
+            //   Console.WriteLine("Введите двузначное число:");
+            //   numbers = Convert.ToInt32(Console.ReadLine());
+
+            //   Console.WriteLine("Число десятков в нем: " + numbers / 10);
+            //   Console.WriteLine("Число единиц в нем: " + numbers);
+
+            //   Console.WriteLine("Сумму его цифр: " + (numbers / 10 + numbers % 10));
+            //   int count = 0;
+            //   for (int i = 1; i <= numbers; i++)
+            //   {
+            //       if (numbers % i == 0)
+            //       {
+            //           count++;
+            //           Console.WriteLine("Произведение его цифр №" + count + ": " + i);
+            //       }
+
+            //   }
+
+
+
+            //   Console.ReadLine();
+
+            //   6)
+
+            //   int Radius;
+            //   int Dlina;
+
+            //   Console.WriteLine("Введите радиус круга:");
+            //   Radius = Convert.ToInt32(Console.ReadLine());
+            //   Console.WriteLine("Введите сторона квадрата:");
+            //   Dlina = Convert.ToInt32(Console.ReadLine());
+
+            //   double Circle;
+            //   double Square;
+            //   Circle = 3.14 * Radius * Radius;
+            //   Square = Dlina * Dlina;
+            //   if (Circle > Square)
+            //   {
+            //       Console.WriteLine("У круга площать больше");
+            //   }
+            //   else
+            //   {
+            //       Console.WriteLine("У квадрата площать больше");
+            //   }
+
+
+            //   Console.ReadLine();
+
+
+            //   7)
+            //   int numbers1;
+            //   int numbers2;
+            //   Console.WriteLine("Введите 1-число:");
+            //   numbers1 = Convert.ToInt32(Console.ReadLine());
+            //   Console.WriteLine("Введите 2-число:");
+            //   numbers2 = Convert.ToInt32(Console.ReadLine());
+            //   Console.Write("Bсе целые числа от 20 до 35: " + 20);
+            //   for (int i = 21; i <= 35; i++)
+            //   {
+            //       Console.Write("," + i);
+            //   }
+            //   Console.WriteLine("\n");
+            //   Console.Write("Kвадраты всех целых чисел от 10 до numbers : " + 100);
+
+            //   for (int i = 11; i <= numbers1; i++)
+            //   {
+            //       Console.Write("," + i * i);
+            //   }
+            //   Console.WriteLine("\n");
+            //   Console.Write("Tретьи степени всех целых чисел от numbers до 50 : " + numbers2 * numbers2 * numbers2);
+            //   for (int i = numbers2; i <= 50; i++)
+            //   {
+            //       Console.Write("," + i * i * i);
+            //   }
+
+
+            //   Console.WriteLine("\n");
+            //   Console.Write("Bсе целые числа от number1 до number2 : " + numbers1);
+            //   for (int i = numbers1 + 1; i <= numbers2; i++)
+            //   {
+            //       Console.Write("," + i);
+            //   }
+            //   Console.ReadLine();
+
+
+
+
+            //   Practice 1)
+            //int X;
+            //   int y = 0;
+            //   Console.WriteLine("Введите x:");
+            //   X = Convert.ToInt32(Console.ReadLine());
+            //   y = 7 * X * X - 3 * X + 4;
+            //   Console.WriteLine("y = " + y);
+            //   Console.ReadLine();
+            //   2)
+            //   int Radius;
+
+            //   Console.WriteLine("Введите радиус круга:");
+            //   Radius = Convert.ToInt32(Console.ReadLine());
+
+
+            //   double Circle;
+            //   double Dlina;
+            //   Circle = 3.14 * Radius * Radius;
+            //   Dlina = 2 * 3.14 * Radius;
+            //   Console.WriteLine("Длина окружности= " + Dlina);
+            //   Console.WriteLine("Площадь круга= " + Circle);
+            //   Console.ReadLine();
+
+
+
+
+            // Остальные повторяеться от домашки
+
+        }
+    }
+}
